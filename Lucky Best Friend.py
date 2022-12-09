34b701ac835a424d2ae05efeefd959653a5943cf
Pranav Satish Kumar <98470657+TheMegaCoderPro@users.noreply.github.com>
2022-12-09 07:31:40 -0700

    Update Lucky Best Friend.py

--- Lucky Best Friend.py
+++ Lucky Best Friend.py
@@ -6,19 +6,36 @@ root=Tk()
 root.title("Luck Best Friend Wheel")
 root.geometry("500x500")
 
-lbf = Label(root)
+enter_name = Entry(root)
+enter_name.place(relx = 0.5, rely = 0.2, anchor = CENTER)
 
-list1 = ["Gray" , "Sam" , "Merrick" , "Jackson" , "Brooks" , "Manny" , "Callie" , "Paige" , "Chris" , "Ace" , "Jeremiah"]
+friend_list = Label(root)
+random_number_label = Label(root)
+lucky_friend = Label(root)
+list1 = ["Gray","Sam","Merrick","Jackson","Brooks","Manny","Callie","Paige","Chris","Ace","Jeremiah","James"]
 print(list1)
 
-def random_number():
-	random_no = random.randint(0, 10)
-	print(random_no)
-	random_lucky_friend = list1[random_no]
-	print("Your Lucky Best Friend Is: " + random_lucky_friend)
-    lbf["text"] = "Your Lucky Best Friend Is: " + random_lucky_friend
+def addfriend():
+    friend_name = enter_name.get()
+    list1.append(friend_name)
+    friend_list["text"] = "Your Friend List: " + str(list1)
 
-btn1 = Button(root, text="Who is your Lucky Best Friend?  ", command = random_number)
+def random_number():
+    length = len(list1)
+	random_no = random.randint(0, length-1)
+    random_number_label["text"] = str(random_no)   
+	generated_random_number = list1[random_no]
+	lucky_friend["text"] = "Your Lucky Best Friend Is: " + str(genrated_random_number)
+
+btn = Button(root, text="Add To Friend List", command = addfriend)
+btn1.place(relx= 0.5,rely = 0.3, anchor = CENTER )
+
+friend_list.place(relx = 0.5, rely = 0.4, anchor = CENTER)
+
+btn1 = Button(root, text="Who is your Lucky Best Friend?", command = random_number)
 btn1.place(relx= 0.5,rely = 0.5, anchor = CENTER )
+
+random_number_label.place(relx = 0.5, rely = 0.6, anchor = CENTER)
+lucky_friend.place(relx = 0.5, rely = 0.7, anchor = CENTER)
 
 root.mainloop()
